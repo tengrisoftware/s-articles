6904b01df22b2d0e42fc5f7a8f10967f053eb50a
Oleg Sviatchenko <oleg.sviatchenko@gmail.com>
2015-05-13 09:54:31 +0600

    bower.json; change connections options, remove waterline from packege.json

--- config/connections.js
+++ config/connections.js
@@ -60,8 +60,8 @@ module.exports.connections = {
     adapter: 'sails-mongo',
     host: 'localhost',
     port: 27017,
-    user: 'mongodb',
-    password: 's1art2',
+    // user: 'mongodb',
+    // password: 's1art2',
     database: 'articles_database'
   }
 
